221eca634863782a7bfa009f70f5eb452923ad89
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2025-01-06 17:47:54 +0300

    feat: add solutions for tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  for (item of domains) {
+    let arr = item.split(".").reverse();
+    let key = "";
+    for (let elem in arr) {
+      key = key.concat(`.${arr[elem]}`);
+      result[key] = Object.hasOwn(result, key) ? ++result[key] : 1;
+    }
+  }
+  return result;
 }
 
 module.exports = {
